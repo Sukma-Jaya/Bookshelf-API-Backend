3a2a5110c12041d7ed7cd406958d641481c7da43
Sukma Jaya <sayajaya1212@gmail.com>
2024-01-04 14:49:06 +0700

    Added full projects Bookshelf API from IDCamp 2023 Scholarship BackEnd

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addBookhandler,
+  getAllBooksHandler,
+  getBooksByIDHandler,
+  editBooksByIdHandler,
+  deleteBooksByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookhandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBooksByIDHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBooksByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBooksByIdHandler,
+  },
+];
+
+module.exports = routes;
